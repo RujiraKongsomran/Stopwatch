8ecff659f75352ae02922484184fe9830df241ff
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-08-14 20:20:15 +0700

    Add animation to view

--- app/src/main/java/com/rujirakongsomran/stopwatch/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/stopwatch/MainActivity.java
@@ -3,12 +3,33 @@ package com.rujirakongsomran.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.rujirakongsomran.stopwatch.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    Animation animSplash, animText, animBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+//        setContentView(R.layout.activity_main);
+
+        // load animation
+        animSplash = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_splash);
+        animText = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_text);
+        animBtn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_btn);
+        // pass animation
+        binding.ivSplash.setAnimation(animSplash);
+        binding.tvTopic.setAnimation(animText);
+        binding.tvContent.setAnimation(animText);
+        binding.btnStart.setAnimation(animBtn);
     }
 }
